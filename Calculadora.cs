5dd4e2c02ea2884f4f64f62d99fd365cf1b5b494
Igor Costa <igor.costa@corp.distrifilm.com.br>
2022-02-18 17:04:15 -0300

    Calculadora: Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Calculator   
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Console.WriteLine("Deseja fazer mais alguma operação?");
+            Console.Write("Digite Sim ou Não:");
+            bool escolha = Convert.ToBoolean(Console.ReadLine());
+            
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("");
+            Console.Write("Escolha a opção desejada: ");
+
+            int opcao = Int32.Parse(Console.ReadLine());
+      
+            switch (opcao)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Multiplicacao(); break;
+                case 4: Divisao(); break;
+            }
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação de Soma");
+            Console.WriteLine("");
+            Console.Write("Digite o primeiro número: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("Digite o segundo número: ");
+            double valor2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            double resultado = valor1 + valor2;
+            Console.WriteLine("O resultado é: " + resultado);
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação de Subtração");
+            Console.WriteLine("");
+            Console.Write("Digite o primeiro número: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("Digite o segundo número: ");
+            double valor2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            double resultado = valor1 - valor2;
+            Console.WriteLine("O resultado é :" + resultado);
+            Console.ReadKey();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação de Multiplicação");
+            Console.WriteLine("");
+            Console.Write("Digite o primeiro número: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("Digite o segundo número: ");
+            double valor2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            double resultado = valor1 * valor2;
+            Console.WriteLine("O resultado é :" + resultado);
+            Console.ReadKey();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Operação de Divisão");
+            Console.WriteLine("");
+            Console.Write("Digite o primeiro número: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("Digite o segundo número: ");
+            double valor2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            double resultado = valor1 / valor2;
+            Console.WriteLine("O resultado é :" + resultado);
+            Console.ReadKey();
+        }
+
+    }
+}
