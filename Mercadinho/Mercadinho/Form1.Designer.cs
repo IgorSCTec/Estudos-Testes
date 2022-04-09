e376ab7b4a773cdca514ca7c811d271745cda894
IgorSCTec <isilvacostatec@gmail.com>
2022-04-09 14:41:20 -0300

    Aula de Técnicas de Programação - Mercadinho

---
+++ Mercadinho/Mercadinho/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Mercadinho
+{
+    partial class lblValor
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chbRapadura = new System.Windows.Forms.CheckBox();
+            this.chbInhame = new System.Windows.Forms.CheckBox();
+            this.chbCupuacu = new System.Windows.Forms.CheckBox();
+            this.chbSandalias = new System.Windows.Forms.CheckBox();
+            this.chbBolacha = new System.Windows.Forms.CheckBox();
+            this.chbSabao = new System.Windows.Forms.CheckBox();
+            this.chbPinico = new System.Windows.Forms.CheckBox();
+            this.lblProdutos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl300 = new System.Windows.Forms.Label();
+            this.lbl1235 = new System.Windows.Forms.Label();
+            this.lbl110 = new System.Windows.Forms.Label();
+            this.lbl185 = new System.Windows.Forms.Label();
+            this.lbl080 = new System.Windows.Forms.Label();
+            this.lbl1117 = new System.Windows.Forms.Label();
+            this.lbl150 = new System.Windows.Forms.Label();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.txtRapadura = new System.Windows.Forms.TextBox();
+            this.txtSandalias = new System.Windows.Forms.TextBox();
+            this.txtPinico = new System.Windows.Forms.TextBox();
+            this.txtSabao = new System.Windows.Forms.TextBox();
+            this.txtBolacha = new System.Windows.Forms.TextBox();
+            this.txtCupuacu = new System.Windows.Forms.TextBox();
+            this.txtInhame = new System.Windows.Forms.TextBox();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chbRapadura
+            // 
+            this.chbRapadura.AutoSize = true;
+            this.chbRapadura.Location = new System.Drawing.Point(35, 62);
+            this.chbRapadura.Name = "chbRapadura";
+            this.chbRapadura.Size = new System.Drawing.Size(73, 17);
+            this.chbRapadura.TabIndex = 0;
+            this.chbRapadura.Text = "Rapadura";
+            this.chbRapadura.UseVisualStyleBackColor = true;
+            // 
+            // chbInhame
+            // 
+            this.chbInhame.AutoSize = true;
+            this.chbInhame.Location = new System.Drawing.Point(35, 99);
+            this.chbInhame.Name = "chbInhame";
+            this.chbInhame.Size = new System.Drawing.Size(61, 17);
+            this.chbInhame.TabIndex = 1;
+            this.chbInhame.Text = "Inhame";
+            this.chbInhame.UseVisualStyleBackColor = true;
+            // 
+            // chbCupuacu
+            // 
+            this.chbCupuacu.AutoSize = true;
+            this.chbCupuacu.Location = new System.Drawing.Point(35, 138);
+            this.chbCupuacu.Name = "chbCupuacu";
+            this.chbCupuacu.Size = new System.Drawing.Size(69, 17);
+            this.chbCupuacu.TabIndex = 2;
+            this.chbCupuacu.Text = "Cupuaçu";
+            this.chbCupuacu.UseVisualStyleBackColor = true;
+            // 
+            // chbSandalias
+            // 
+            this.chbSandalias.AutoSize = true;
+            this.chbSandalias.Location = new System.Drawing.Point(35, 177);
+            this.chbSandalias.Name = "chbSandalias";
+            this.chbSandalias.Size = new System.Drawing.Size(72, 17);
+            this.chbSandalias.TabIndex = 3;
+            this.chbSandalias.Text = "Sandálias";
+            this.chbSandalias.UseVisualStyleBackColor = true;
+            // 
+            // chbBolacha
+            // 
+            this.chbBolacha.AutoSize = true;
+            this.chbBolacha.Location = new System.Drawing.Point(35, 216);
+            this.chbBolacha.Name = "chbBolacha";
+            this.chbBolacha.Size = new System.Drawing.Size(65, 17);
+            this.chbBolacha.TabIndex = 4;
+            this.chbBolacha.Text = "Bolacha";
+            this.chbBolacha.UseVisualStyleBackColor = true;
+            // 
+            // chbSabao
+            // 
+            this.chbSabao.AutoSize = true;
+            this.chbSabao.Location = new System.Drawing.Point(35, 256);
+            this.chbSabao.Name = "chbSabao";
+            this.chbSabao.Size = new System.Drawing.Size(57, 17);
+            this.chbSabao.TabIndex = 5;
+            this.chbSabao.Text = "Sabão";
+            this.chbSabao.UseVisualStyleBackColor = true;
+            // 
+            // chbPinico
+            // 
+            this.chbPinico.AutoSize = true;
+            this.chbPinico.Location = new System.Drawing.Point(35, 294);
+            this.chbPinico.Name = "chbPinico";
+            this.chbPinico.Size = new System.Drawing.Size(55, 17);
+            this.chbPinico.TabIndex = 6;
+            this.chbPinico.Text = "Pinico";
+            this.chbPinico.UseVisualStyleBackColor = true;
+            // 
+            // lblProdutos
+            // 
+            this.lblProdutos.AutoSize = true;
+            this.lblProdutos.Location = new System.Drawing.Point(51, 29);
+            this.lblProdutos.Name = "lblProdutos";
+            this.lblProdutos.Size = new System.Drawing.Size(49, 13);
+            this.lblProdutos.TabIndex = 7;
+            this.lblProdutos.Text = "Produtos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Valor";
+            // 
+            // lbl300
+            // 
+            this.lbl300.AutoSize = true;
+            this.lbl300.Location = new System.Drawing.Point(178, 100);
+            this.lbl300.Name = "lbl300";
+            this.lbl300.Size = new System.Drawing.Size(45, 13);
+            this.lbl300.TabIndex = 9;
+            this.lbl300.Text = "R$ 3.00";
+            // 
+            // lbl1235
+            // 
+            this.lbl1235.AutoSize = true;
+            this.lbl1235.Location = new System.Drawing.Point(178, 178);
+            this.lbl1235.Name = "lbl1235";
+            this.lbl1235.Size = new System.Drawing.Size(51, 13);
+            this.lbl1235.TabIndex = 10;
+            this.lbl1235.Text = "R$ 12.35";
+            // 
+            // lbl110
+            // 
+            this.lbl110.AutoSize = true;
+            this.lbl110.Location = new System.Drawing.Point(178, 139);
+            this.lbl110.Name = "lbl110";
+            this.lbl110.Size = new System.Drawing.Size(45, 13);
+            this.lbl110.TabIndex = 11;
+            this.lbl110.Text = "R$ 1.10";
+            // 
+            // lbl185
+            // 
+            this.lbl185.AutoSize = true;
+            this.lbl185.Location = new System.Drawing.Point(178, 217);
+            this.lbl185.Name = "lbl185";
+            this.lbl185.Size = new System.Drawing.Size(45, 13);
+            this.lbl185.TabIndex = 12;
+            this.lbl185.Text = "R$ 1.85";
+            // 
+            // lbl080
+            // 
+            this.lbl080.AutoSize = true;
+            this.lbl080.Location = new System.Drawing.Point(178, 257);
+            this.lbl080.Name = "lbl080";
+            this.lbl080.Size = new System.Drawing.Size(45, 13);
+            this.lbl080.TabIndex = 13;
+            this.lbl080.Text = "R$ 0.80";
+            // 
+            // lbl1117
+            // 
+            this.lbl1117.AutoSize = true;
+            this.lbl1117.Location = new System.Drawing.Point(178, 295);
+            this.lbl1117.Name = "lbl1117";
+            this.lbl1117.Size = new System.Drawing.Size(51, 13);
+            this.lbl1117.TabIndex = 14;
+            this.lbl1117.Text = "R$ 11.17";
+            // 
+            // lbl150
+            // 
+            this.lbl150.AutoSize = true;
+            this.lbl150.Location = new System.Drawing.Point(178, 63);
+            this.lbl150.Name = "lbl150";
+            this.lbl150.Size = new System.Drawing.Size(45, 13);
+            this.lbl150.TabIndex = 15;
+            this.lbl150.Text = "R$ 1.50";
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(301, 29);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(62, 13);
+            this.lblQuantidade.TabIndex = 16;
+            this.lblQuantidade.Text = "Quantidade";
+            // 
+            // txtRapadura
+            // 
+            this.txtRapadura.Location = new System.Drawing.Point(283, 60);
+            this.txtRapadura.Name = "txtRapadura";
+            this.txtRapadura.Size = new System.Drawing.Size(100, 20);
+            this.txtRapadura.TabIndex = 17;
+            this.txtRapadura.Tag = "";
+            // 
+            // txtSandalias
+            // 
+            this.txtSandalias.Location = new System.Drawing.Point(283, 175);
+            this.txtSandalias.Name = "txtSandalias";
+            this.txtSandalias.Size = new System.Drawing.Size(100, 20);
+            this.txtSandalias.TabIndex = 18;
+            this.txtSandalias.Tag = "";
+            // 
+            // txtPinico
+            // 
+            this.txtPinico.Location = new System.Drawing.Point(283, 291);
+            this.txtPinico.Name = "txtPinico";
+            this.txtPinico.Size = new System.Drawing.Size(100, 20);
+            this.txtPinico.TabIndex = 19;
+            this.txtPinico.Tag = "";
+            // 
+            // txtSabao
+            // 
+            this.txtSabao.Location = new System.Drawing.Point(283, 254);
+            this.txtSabao.Name = "txtSabao";
+            this.txtSabao.Size = new System.Drawing.Size(100, 20);
+            this.txtSabao.TabIndex = 20;
+            this.txtSabao.Tag = "";
+            // 
+            // txtBolacha
+            // 
+            this.txtBolacha.Location = new System.Drawing.Point(283, 214);
+            this.txtBolacha.Name = "txtBolacha";
+            this.txtBolacha.Size = new System.Drawing.Size(100, 20);
+            this.txtBolacha.TabIndex = 21;
+            this.txtBolacha.Tag = "";
+            // 
+            // txtCupuacu
+            // 
+            this.txtCupuacu.Location = new System.Drawing.Point(283, 136);
+            this.txtCupuacu.Name = "txtCupuacu";
+            this.txtCupuacu.Size = new System.Drawing.Size(100, 20);
+            this.txtCupuacu.TabIndex = 22;
+            this.txtCupuacu.Tag = "";
+            // 
+            // txtInhame
+            // 
+            this.txtInhame.Location = new System.Drawing.Point(283, 97);
+            this.txtInhame.Name = "txtInhame";
+            this.txtInhame.Size = new System.Drawing.Size(100, 20);
+            this.txtInhame.TabIndex = 23;
+            this.txtInhame.Tag = "";
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(156, 360);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(105, 23);
+            this.btnComprar.TabIndex = 24;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(167, 401);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(76, 13);
+            this.lblResultado.TabIndex = 25;
+            this.lblResultado.Text = "Total a pagar :";
+            // 
+            // lblValor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 445);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.txtInhame);
+            this.Controls.Add(this.txtCupuacu);
+            this.Controls.Add(this.txtBolacha);
+            this.Controls.Add(this.txtSabao);
+            this.Controls.Add(this.txtPinico);
+            this.Controls.Add(this.txtSandalias);
+            this.Controls.Add(this.txtRapadura);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.lbl150);
+            this.Controls.Add(this.lbl1117);
+            this.Controls.Add(this.lbl080);
+            this.Controls.Add(this.lbl185);
+            this.Controls.Add(this.lbl110);
+            this.Controls.Add(this.lbl1235);
+            this.Controls.Add(this.lbl300);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblProdutos);
+            this.Controls.Add(this.chbPinico);
+            this.Controls.Add(this.chbSabao);
+            this.Controls.Add(this.chbBolacha);
+            this.Controls.Add(this.chbSandalias);
+            this.Controls.Add(this.chbCupuacu);
+            this.Controls.Add(this.chbInhame);
+            this.Controls.Add(this.chbRapadura);
+            this.Name = "lblValor";
+            this.Text = "Bolacha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chbRapadura;
+        private System.Windows.Forms.CheckBox chbInhame;
+        private System.Windows.Forms.CheckBox chbCupuacu;
+        private System.Windows.Forms.CheckBox chbSandalias;
+        private System.Windows.Forms.CheckBox chbBolacha;
+        private System.Windows.Forms.CheckBox chbSabao;
+        private System.Windows.Forms.CheckBox chbPinico;
+        private System.Windows.Forms.Label lblProdutos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl300;
+        private System.Windows.Forms.Label lbl1235;
+        private System.Windows.Forms.Label lbl110;
+        private System.Windows.Forms.Label lbl185;
+        private System.Windows.Forms.Label lbl080;
+        private System.Windows.Forms.Label lbl1117;
+        private System.Windows.Forms.Label lbl150;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.TextBox txtRapadura;
+        private System.Windows.Forms.TextBox txtSandalias;
+        private System.Windows.Forms.TextBox txtPinico;
+        private System.Windows.Forms.TextBox txtSabao;
+        private System.Windows.Forms.TextBox txtBolacha;
+        private System.Windows.Forms.TextBox txtCupuacu;
+        private System.Windows.Forms.TextBox txtInhame;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
