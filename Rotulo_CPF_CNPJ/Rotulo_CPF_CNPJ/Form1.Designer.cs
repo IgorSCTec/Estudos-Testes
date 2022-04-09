e9cd2fb81546d51ed3c71d1364d4236e48cb4b95
IgorSCTec <isilvacostatec@gmail.com>
2022-04-09 14:42:03 -0300

    Aula de Técnicas de Programação - Rotulo_CPF_CNPJ

---
+++ Rotulo_CPF_CNPJ/Rotulo_CPF_CNPJ/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Rotulo_CPF_CNPJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblCNPJ = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(12, 40);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(84, 13);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Tipo de Pessoa:";
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "Física",
+            "Jurídica"});
+            this.cbTipo.Location = new System.Drawing.Point(102, 37);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(93, 21);
+            this.cbTipo.TabIndex = 1;
+            this.cbTipo.SelectedIndexChanged += new System.EventHandler(this.cbTipo_SelectedIndexChanged);
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(201, 41);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(30, 13);
+            this.lblCPF.TabIndex = 2;
+            this.lblCPF.Text = "CPF:";
+            this.lblCPF.Visible = false;
+            // 
+            // lblCNPJ
+            // 
+            this.lblCNPJ.AutoSize = true;
+            this.lblCNPJ.Location = new System.Drawing.Point(201, 41);
+            this.lblCNPJ.Name = "lblCNPJ";
+            this.lblCNPJ.Size = new System.Drawing.Size(37, 13);
+            this.lblCNPJ.TabIndex = 3;
+            this.lblCNPJ.Text = "CNPJ:";
+            this.lblCNPJ.Visible = false;
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(244, 38);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(155, 20);
+            this.txtDocumento.TabIndex = 4;
+            this.txtDocumento.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 95);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.lblCNPJ);
+            this.Controls.Add(this.lblCPF);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.lblTipo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox cbTipo;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblCNPJ;
+        private System.Windows.Forms.TextBox txtDocumento;
+    }
+}
+
