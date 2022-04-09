c34d41dc550db3a278179ea0ebeaf3569600c06b
IgorSCTec <isilvacostatec@gmail.com>
2022-04-09 14:39:19 -0300

    Aula de Técnicas de Programação - Aumento de Salário

---
+++ AumentodeSalario/AumentodeSalario/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace AumentodeSalario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.rtbResultado = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(21, 41);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(88, 13);
+            this.lblCargo.TabIndex = 0;
+            this.lblCargo.Text = "Escolha o Cargo:";
+            // 
+            // cbCargo
+            // 
+            this.cbCargo.FormattingEnabled = true;
+            this.cbCargo.Items.AddRange(new object[] {
+            "Gerente",
+            "Técnico",
+            "Auxiliar",
+            "Outros"});
+            this.cbCargo.Location = new System.Drawing.Point(128, 41);
+            this.cbCargo.Name = "cbCargo";
+            this.cbCargo.Size = new System.Drawing.Size(123, 21);
+            this.cbCargo.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(21, 77);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(21, 114);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(42, 13);
+            this.lblSalario.TabIndex = 3;
+            this.lblSalario.Text = "Salário:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(128, 77);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(123, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(128, 114);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(123, 20);
+            this.txtSalario.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(33, 151);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 27);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(156, 151);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(95, 27);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar Dados";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // rtbResultado
+            // 
+            this.rtbResultado.Location = new System.Drawing.Point(33, 193);
+            this.rtbResultado.Name = "rtbResultado";
+            this.rtbResultado.Size = new System.Drawing.Size(218, 132);
+            this.rtbResultado.TabIndex = 8;
+            this.rtbResultado.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 337);
+            this.Controls.Add(this.rtbResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.cbCargo);
+            this.Controls.Add(this.lblCargo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.ComboBox cbCargo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.RichTextBox rtbResultado;
+    }
+}
+
---
+++ AumentodeSalario/AumentodeSalario/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AumentodeSalario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(cbCargo.Text == "Gerente")
+            {
+                if (txtSalario.Text == "" || txtNome.Text == "")
+                {
+                    MessageBox.Show("Preencha todas as informações");
+                }
+                else
+                {
+                    var salarioAntigo = double.Parse(txtSalario.Text);
+                    double aumento = double.Parse(txtSalario.Text) * 0.05;
+                    rtbResultado.Text = $"Nome: {txtNome.Text}\nCargo: {cbCargo.Text}\nSalário: {salarioAntigo}\nAumento: 5%\nTotal: {(salarioAntigo + aumento)}\n_____________________________";
+                }
+            }
+            if (cbCargo.Text == "Técnico")
+            {
+                if (txtSalario.Text == "" || txtNome.Text == "")
+                {
+                    MessageBox.Show("Preencha todas as informações");
+                }
+                else
+                {
+                    var salarioAntigo = double.Parse(txtSalario.Text);
+                    double aumento = double.Parse(txtSalario.Text) * 0.075;
+                    rtbResultado.Text = $"Nome: {txtNome.Text}\nCargo: {cbCargo.Text}\nSalário: {salarioAntigo}\nAumento: 7,5%\nTotal: {(salarioAntigo + aumento)}\n_____________________________";
+                }
+            }
+            if (cbCargo.Text == "Auxiliar")
+            {
+                if (txtSalario.Text == "" || txtNome.Text == "")
+                {
+                    MessageBox.Show("Preencha todas as informações");
+                }
+                else
+                {
+                    var salarioAntigo = double.Parse(txtSalario.Text);
+                    double aumento = double.Parse(txtSalario.Text) * 0.10;
+                    rtbResultado.Text = $"Nome: {txtNome.Text}\nCargo: {cbCargo.Text}\nSalário: {salarioAntigo}\nAumento: 10%\nTotal: {(salarioAntigo + aumento)}\n_____________________________";
+                }
+            }
+            if (cbCargo.Text == "Outros")
+            {
+                if (txtSalario.Text == "" || txtNome.Text == "")
+                {
+                    MessageBox.Show("Preencha todas as informações");
+                }
+                else
+                {
+                    var salarioAntigo = double.Parse(txtSalario.Text);
+                    double aumento = double.Parse(txtSalario.Text) * 0.04;
+                    rtbResultado.Text = $"Nome: {txtNome.Text}\nCargo: {cbCargo.Text}\nSalário: {salarioAntigo}\nAumento: 4%\nTotal: {(salarioAntigo + aumento)}\n_____________________________";
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            cbCargo.Text = "";
+            txtNome.Text = "";
+            txtSalario.Text = "";
+            rtbResultado.Text = "";
+        }
+    }
+}
